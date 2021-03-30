f7e040f9b2bc1911622a62f95ded7a5f9f772e13
Nala Ginrut <mulei@gnu.org>
2021-03-30 14:35:23 +0800

    Make test suite more elegantly

--- tests/test.scm
+++ tests/test.scm
@@ -3,32 +3,31 @@
   #:use-module (rnrs)
   #:use-module (laco compile))
 
-(define (gen-lef filename)
-  (string-append "/dev/shm/"
-                 (substring/shared filename 0 (string-index filename #\.))))
-
-(define (lambdachip-run filename)
-  (let ((tf (format #f "tests/scm/~a" filename)))
+(define (lambdachip-run name)
+  (let ((tf (format #f "tests/scm/~a.scm" name)))
     (when (not (file-exists? tf))
       (format #t "The test file `~a' is missing!" tf)
       (exit -1))
     (let ((vm (getenv "LAMBDACHIP_VM_PATH"))
-          (lef (gen-lef filename)))
+          (lef (format #f "/tmp/~a.lef" name)))
       (when (not vm)
         (format #t "Please set LAMBDACHIP_VM_PATH then try again!")
         (exit -2))
       (system (format #f "./pre-inst-env scripts/laco ~a -o ~a >/dev/null" tf lef))
-      (system (format #f "~a/lambdachip-vm ~a > /dev/shm/mmr.log" vm lef))
-      (call-with-input-file "/dev/shm/mmr.log" get-string-all))))
+      (system (format #f "~a/lambdachip-vm ~a > /tmp/~a.log" vm lef name))
+      (call-with-input-file (format #f "/tmp/~a.log" name) get-string-all))))
+
+(define (get-result name)
+  (call-with-input-file (format #f "tests/result/~a.txt" name) get-string-all))
+
+(define (check case name)
+  (test-equal case
+    (get-result name)
+    (lambdachip-run name)))
 
 (test-begin "test-suite")
 
-(test-equal "Fibonacci"
-  "120"
-  (lambdachip-run "fibonacci.scm"))
-
-(test-equal "Nested calling"
-  "2049"
-  (lambdachip-run "nested-call.scm"))
+(check "Fibonacci" "fibonacci")
+(check "Nested calling" "nested-call")
 
 (test-end "test-suite")
