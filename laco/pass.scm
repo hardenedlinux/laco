147beb87ce591d8fd61473b9ebb890faf84b2426
Nala Ginrut <mulei@gnu.org>
2021-06-30 20:16:56 +0800

    Remove debug info

--- laco/pass.scm
+++ laco/pass.scm
@@ -57,13 +57,13 @@
                            (pass last)))
                      (else (fail!))))
                    (else (throw 'laco-error 'run-pass "Invalid pass: `~a'!" 'item)))))
-            (pk "item"(object->string item))
-            (pretty-print
-             (match e
-               ((? cps?) (cps->expr e))
-               ((? insr?) (lir->expr e))
-               ((? primitive?) (primitive-name e))
-               ((? object?) (object->value e))
-               (else (error "BUG: invalid type" e))))
+            ;; (pk "item"(object->string item))
+            ;; (pretty-print
+            ;;  (match e
+            ;;    ((? cps?) (cps->expr e))
+            ;;    ((? insr?) (lir->expr e))
+            ;;    ((? primitive?) (primitive-name e))
+            ;;    ((? object?) (object->value e))
+            ;;    (else (error "BUG: invalid type" e))))
             e))
         expr (list 'lst ...)))
