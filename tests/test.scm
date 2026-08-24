00ed5d65575cb83b7a9d3f8ec49d9c7542ba5501
Nala Ginrut <mulei@gnu.org>
2026-08-25 01:32:45 +0800

    Update tests

--- tests/test.scm
+++ tests/test.scm
@@ -63,6 +63,7 @@
 (check "Named Let 3" "named-let-3")
 (check "Named Let Loop" "named-let-loop")
 (check "Named Let Loop in Function" "named-let-loop-in-func")
+(check "Let var naming" "let-var-naming")
 (check "Applicative order 1" "applicative-order-1")
 (check "list-ref" "list-ref")
 (check "add_sub" "add_sub")
